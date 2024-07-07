9f58207400e1ffc705db579e078cec395875d556
Abiud-lang <abiud.kiprotich@student.moringaschool.com>
2024-07-07 17:38:26 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const button = document.getElementById('button');
+    function handleClick(event) {
+        alert = "Click Me!"
+    }
+    button.addEventListener("Click Me!", handleClick)
+
 }
